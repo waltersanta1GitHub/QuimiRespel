f621fad8753e1a47574f869daf15baaff224e386
Nicolás Miranda <nicolasmirandam@outlook.com>
2017-11-22 18:59:56 -0500

    Formatos 8 y 9... OK

--- codigo/Quimirespel/Quimirespel/Formularios/Transporte/ManifiestoCarga.aspx
+++ codigo/Quimirespel/Quimirespel/Formularios/Transporte/ManifiestoCarga.aspx
@@ -1,3 +1,4 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="ManifiestoCarga.aspx.cs" Inherits="Quimirespel.Formularios.Transporte.ManifiestoCarga" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <asp:Button ID="btnImprimir" runat="server" OnClick="btnImprimir_Click" Text="Imprimir Formulario" />
 </asp:Content>
--- codigo/Quimirespel/Quimirespel/Formularios/Transporte/ProgramacionServicio.aspx.designer.cs
+++ codigo/Quimirespel/Quimirespel/Formularios/Transporte/ProgramacionServicio.aspx.designer.cs
@@ -11,5 +11,14 @@ namespace Quimirespel.Formularios.Transporte {
     
     
     public partial class ProgramacionServicio {
+        
+        /// <summary>
+        /// btnImprimir control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnImprimir;
     }
 }
