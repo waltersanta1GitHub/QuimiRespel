bf7823a1f3bcdf57471063cd8b64f8b236ada9d5
Nicolás Miranda <nicolasmirandam@outlook.com>
2017-12-07 18:36:32 -0500

    Creación de nuevo proyecto MVC para trabajar con la biblioteca Select.HtmlToPdf

---
+++ codigo/Quimirespel/ConversorPDF/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using SelectPdf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ConversorPDF.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SubmitAction(FormCollection collection)
+        {
+
+            string text = System.IO.File.ReadAllText(@"C:\Users\Juan Pablo\Desktop\ProyectoWalter\Plantillas\" + collection["TxtPath"]);
+
+            byte[] pdf = convertir(text);
+            FileResult fileResult = new FileContentResult(pdf, "application/pdf");
+            fileResult.FileDownloadName = "Document.pdf";
+            return fileResult;
+        }
+
+        public byte[] convertir(string texto)
+        {
+            var convertir = new HtmlToPdf();
+            PdfDocument documentoPdf = convertir.ConvertHtmlString(texto);
+
+            byte[] pdf = documentoPdf.Save();
+            documentoPdf.Close();
+
+            return pdf;
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ codigo/Quimirespel/ConversorPDF/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ConversorPDF.Startup))]
+namespace ConversorPDF
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
